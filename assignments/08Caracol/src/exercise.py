a7e0b790c228886cf5ebfd190252b62e6c06b602
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-17 17:54:34 -0500

    Solved!

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,7 +2,7 @@ def main():
     #escribe tu código abajo de esta línea
     minutos= float(input("Dame los minutos: "))
     centimetros= (60*5.7) / 10
-    total= centimetros*minutos
+    total= float(centimetros*minutos)
     
     print("Centímetros recorridos:", total)
 
