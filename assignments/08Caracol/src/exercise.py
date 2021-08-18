92b2c73ec0c4b8b3eb874ed75d3e0eeb057ac517
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-17 23:13:58 -0500

    Solved!

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,10 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
     minutos= float(input("Dame los minutos: "))
-    centimetros= (60*5.7) / 10
-    total= (centimetros*minutos)
+    centimetros= ((60*5.7) / 10) * minutos
     
-    print("Centímetros recorridos:" + str(total))
+    print("Centímetros recorridos:", centimetros)
 
 if __name__ == '__main__':
     main()
