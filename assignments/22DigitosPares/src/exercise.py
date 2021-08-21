43ce7129a315b1c4c584662fb5df3574330c92ae
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-20 21:40:47 -0500

    Solved!

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,14 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
     numero= int(input("Dame un número: "))
-    even_num= 0
+    pares= 0
 
     while(numero>0):
         if numero%2== 0:
-            even_num+=1
+            pares+=1
+        else:
+            pares+=0
         numero= numero//10
 
-    print("El número de digitos pares es:", even_num)
+    print("El número de digitos pares es:", pares)
 
 
 if __name__ == '__main__':
