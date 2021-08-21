0fe21df04fa50455b7b78259fe037cf4643d439a
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-20 21:05:43 -0500

    Solved!

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,8 +2,9 @@ def main():
     #escribe tu código abajo de esta línea
     minutos= float(input("Dame los minutos: "))
     centimetros= (((5.7*60) /10) * minutos)
+    final= round(centimetros,1)
     
-    print("Centímentros recorridos:", centimetros)
+    print("Centímentros recorridos:", final)
 
 if __name__ == '__main__':
     main()
