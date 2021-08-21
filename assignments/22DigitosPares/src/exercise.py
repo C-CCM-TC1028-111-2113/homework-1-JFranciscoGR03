38fd5b9cdad5d5c0109e2965d742e8680039f966
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-20 22:00:55 -0500

    Solved!

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,14 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-    numero= int(input("Dame un número: "))
+    num= int(input("Dame un número: "))
+    lista= list(str(num))
+    dig= (int(i) for i in lista)
     pares= 0
-
-    while(numero>0):
-        if numero%2== 0:
+    for num in dig:
+        if num%2== 0:
             pares+=1
         else:
             pares+=0
-        numero= numero//10
 
     print("El número de digitos pares es:", pares)
 
