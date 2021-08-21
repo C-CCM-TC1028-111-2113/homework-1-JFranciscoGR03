589316c82b7521a9a5f2b493893b6db25efc6d5d
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-20 21:29:50 -0500

    Solved!

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -4,10 +4,10 @@ def main():
     palabras= int(input("Dame el número de palabras: "))
     
     paginas= math.ceil((palabras/475))
-    costo= paginas*60
-    costoDescuento= costo - (costo*0.1)
+    costo= (paginas*60)
+    costo= costo-(costo*0.1)
     
-    print("El costo de la publicación es: ", costoDescuento)
+    print("El costo de la publicación es: ", costo)
 
 
 if __name__ == '__main__':
